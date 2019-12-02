0e6663653138c391051bba9151cb5d589255746d
bunchley <bunchley007@gmail.com>
2019-12-02 11:37:02 -0700

    calculator functioning

--- script/main.js
+++ script/main.js
@@ -1,7 +1,73 @@
-let action = "";
+let numbers = document.querySelectorAll(".number");
+let displayBox = document.querySelector(".display");
+let clearButton = document.querySelector(".clear");
+let operators = document.querySelectorAll(".operator");
+let equalButton = document.querySelector(".equal");
+let negativeButton = document.querySelector(".negative");
+let currentOperator = "";
+let secondNumber = "";
+let firstNumber = "";
+let total = "";
 
+displayNumber();
+function displayNumber() {
+  numbers.forEach(number => {
+    number.addEventListener("click", () => {
+      if (currentOperator != "" && secondNumber == "") {
+        displayBox.textContent = "";
+      }
+      displayBox.textContent = displayBox.textContent + number.textContent;
+      secondNumber = displayBox.textContent;
+    });
+  });
+}
+clearDisplay();
+function clearDisplay() {
+  clearButton.addEventListener("click", () => {
+    displayBox.textContent = "";
+    firstNumber = "";
+    secondNumber = "";
+    total = "";
+  });
+}
+selectOperator();
+function selectOperator() {
+  operators.forEach(operator => {
+    operator.addEventListener("click", () => {
+      currentOperator = operator.textContent;
+      console.log(currentOperator);
+      firstNumber = displayBox.textContent;
+      console.log(firstNumber, ", firstNum");
+      secondNumber = "";
+    });
+  });
+}
+
+computeTotal();
+function computeTotal() {
+  equalButton.addEventListener("click", () => {
+    secondNumber = displayBox.textContent;
+    console.log(displayBox.textContent, ", secondNumber");
+    total = operate(currentOperator, firstNumber, secondNumber);
+
+    console.log(total, ", total");
+    firstNumber = total;
+    console.log(firstNumber, ", firstNum");
+    currentOperator = "";
+    displayBox.textContent = total;
+  });
+}
+
+toggleNegative();
+function toggleNegative() {
+  negativeButton.addEventListener("click", () => {
+    displayBox.textContent = -displayBox.textContent;
+  });
+}
+
+// OPERATOR FUNCTIONS - START
 function addition(num1, num2) {
-  return num1 + num2;
+  return parseInt(num1) + parseInt(num2);
 }
 
 function subtraction(num1, num2) {
@@ -17,16 +83,20 @@ function division(num1, num2) {
 }
 
 function operate(action, num1, num2) {
-  if (action === "add") {
+  if (action === "+") {
     return addition(num1, num2);
   }
-  if (action === "subtract") {
+  if (action === "-") {
     return subtraction(num1, num2);
   }
-  if (action === "multiply") {
+  if (action === "x") {
     return multiplication(num1, num2);
   }
-  if (action === "divide") {
+  if (action === "/") {
     return division(num1, num2);
   }
+  if (action === "%") {
+    return parseInt(num1) % parseInt(num2);
+  }
 }
+// OPERATOR FUNCTIONS - END
